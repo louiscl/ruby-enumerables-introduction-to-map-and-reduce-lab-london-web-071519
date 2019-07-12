98c7c47ea422659dcda42349b99def32e9c1f85f
Louis Lammerting <louis.lammerting@gmail.com>
2019-07-12 03:11:16 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -22,7 +22,7 @@ def reduce_to_total(source_array,starting_point=0)
 end  
 
 def reduce_to_all_true(source_array)
-  source_array.all?(true||nil)
+  source_array.none?(false)
 end
 
 def reduce_to_any_true(source_array)
