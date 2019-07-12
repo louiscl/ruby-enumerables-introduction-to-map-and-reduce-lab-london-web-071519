fe2dae078e4bc19c6f5f24221b7560272f2b6aeb
Louis Lammerting <louis.lammerting@gmail.com>
2019-07-12 02:45:28 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -15,7 +15,7 @@ def map_to_square(source_array)
 end
 
 def reduce_to_total(source_array,starting_point=0)
-  new = 0
+  new = starting_point
   source_array.each {|x| new += x}
   starting_point
   return new
